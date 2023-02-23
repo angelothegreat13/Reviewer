d794162838301c2757564aef4d874dee386c694b
Michael Angelo Manaog <angelothegreat13@gmail.com>
2023-02-23 23:18:24 +0800

    add table of contents

---
+++ index.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html>
+<style>
+	* {
+		margin: 0;
+		padding: 0;
+		box-sizing: border-box;
+	}
+	body {
+		background: #1b1b1b;
+		font-family: Arial;
+	}
+	div.container {
+	 	margin: auto;
+	  	text-align: center;
+	 	width: 50%;
+	 	border: 3px solid #ffa31a;
+	 	padding: 25px;
+	}
+	h2.tbl-contents{
+		font-size: 30px;
+		margin-bottom: 10px;
+		color: #ffa31a;
+	}
+	h3.title {
+		font-size: 23px;
+		color: #ffa31a;
+		margin-bottom: 10px;
+	}
+	a {
+  		color: #fff;
+  		text-decoration: none;
+	}
+	a:hover {
+		text-decoration: underline;
+	}
+	ul {
+  		text-align: left;
+  		list-style-type: none;
+  		margin-bottom: 10px;
+	}
+	ul li {
+		font-size: 18px;
+		line-height: 24px;
+	}
+</style>
+<body>
+<br><br><br><br><br><br><br>
+<div class="container">
+	<h2 class="tbl-contents">Table of Contents</h2>
+	<ul><h3 class="title">Object Oriented Programming</h3>
+		<li><a href="#">What is OOP?</a></li>
+		<li><a href="#">Encapsulation</a></li>
+		<li><a href="#">Abstraction</a></li>
+		<li><a href="#">Polymorphism</a></li>
+		<li><a href="#">Inheritance</a></li>
+	</ul>  
+	<ul><h3 class="title">Design Pattern</h3>
+		<li><a href="#">Singleton</a></li>
+		<li><a href="#">Factory Pattern</a></li>
+		<li><a href="#">Facade</a></li>
+		<li><a href="#">Builder</a></li>
+		<li><a href="#">Adapter</a></li>
+	</ul> 
+	<ul><h3 class="title">SOLID Principle</h3>
+		<li><a href="#">Single Responsibility Principle (SRP)</a></li>
+		<li><a href="#">Open-Closed Principle (OCP)</a></li>
+		<li><a href="#">Liskov Substitution Principle (LSP)</a></li>
+		<li><a href="#">Interface Segregation Principle (ISP)</a></li>
+		<li><a href="#">Dependency Inversion Principle (DIP)</a></li>
+	</ul> 
+</div>
+</body>
+</html>
